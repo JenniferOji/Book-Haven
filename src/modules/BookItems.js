0695665098fb562bf0432b68c0d30e90fb546531
JenniferOji <g00413791@atu.ie>
2024-12-02 18:41:03 +0000

    added the books to be displayed

--- src/modules/BookItems.js
+++ src/modules/BookItems.js
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card';
 import axios from "axios";
 //where all the movies are displayed
 const BookItem = (props) => {
-    const { Title, Year, Poster } = props.myBook;
+    const { title, description, cover } = props.myBook;
 //displaying the items of the array 
 //using card is decorative and makes the page look better 
 const handleDelete = (e)=>{
@@ -22,8 +22,8 @@ const handleSubmit = (e) => {
 
     const book = {
         title: props.myBook.title,
-        year: props.myBook.year,
-        poster: props.myBook.poster
+        year: props.myBook.description,
+        poster: props.myBook.cover
       };
     
     axios.post('http://localhost:4000/api/bookList', book)
@@ -31,14 +31,15 @@ const handleSubmit = (e) => {
       .catch((err) => console.log(err.data));
   }
     return(
-        <div class="col-4 mb-4">
-            <div class="card" style={{ width: "30rem", height: "20rem" }}>
-                <h5 class="card-header">{Title}</h5> 
+        <div class="col-4 mb-3">
+            <div class="card" style={{ width: "26rem", height: "20rem", marginLeft:40, marginTop:10}}>
+                <h5 class="card-header">{title}</h5> 
                 <div class="card-body d-flex align-items-center">
-                    <img src={Poster}alt={Title}height={250}width={150}style={{ paddingBottom: "35px" }}/>
-                    <div class="text-container" style={{flex: 1,overflow: "hidden",position: "relative",}}>
-                        <p style={{padding: 8,height: "240px",lineHeight: "1.2",}}>
-                            {Year}
+                    <img src={cover}alt={title}height={250}width={150}style={{ paddingBottom: "35px" }}/>
+                    {/* this container displays the description of the book - theres a scroll wheel in order to view the full description */}
+                    <div class="text-container"  style={{overflowY: "scroll",flex: 1,position: "relative",}}>
+                        <p  style={{padding: 8,height: "240px",lineHeight: "1.2",}}>
+                            {description}
                         </p>
                     </div>
                     <a class="btn btn-success" onClick={handleSubmit} style={{position: "absolute",left: 15,width: "94%",bottom: 6,height: "30px",lineHeight: "10px",}}>Add to List</a>
